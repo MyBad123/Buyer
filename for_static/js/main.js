2c502ca398dde33175d5c0e791efb0da8db33a82
MyBad123 <gena.kyznetsov10@gmail.com>
2022-03-12 16:35:03 +0300

    12.03.2022: fix bugs v1

--- for_static/js/main.js
+++ for_static/js/main.js
@@ -1,6 +1,13 @@
 
 
 for (let i of document.querySelectorAll('.item')) {
+    i.onmouseover = (e) => {
+        e.target.closest(".item").classList.add("table-primary");
+    }
+    i.onmouseout = (e) => {
+        e.target.closest(".item").classList.remove("table-primary");
+    }
+    
     i.onclick = (e) => {
         let refObject = e.target.closest('.item').querySelector('.item-ref').href;
         console.log(refObject);
