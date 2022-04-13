2d763239a73d9142df970ba4b52aa9d63eae92fc
161-github <spgithub@1d61.com>
2022-04-13 17:55:54 +0300

    Add docker-compos configuration and sensitive data output to environment variables.

--- Buyer/celery.py
+++ Buyer/celery.py
@@ -7,4 +7,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Buyer.settings')
 app = Celery('Buyer')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
