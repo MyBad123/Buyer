03ce14b4c8f2b472b5e87c2414df8cccabda3088
Askhab Musaev <a.musaev@tnxholding.com>
2022-05-08 16:29:33 +0300

    database for parser_db

--- htmlTree/__init__.py
+++ htmlTree/__init__.py
@@ -1,3 +1,21 @@
 import os
 
+from django.db import connection
+
+cursor = connection.cursor()
+cursor.execute(
+    '''
+    SELECT datname FROM pg_catalog.pg_database WHERE datname='parser_db'
+    '''
+)
+result = cursor.fetchone()
+if not result:
+    cursor.execute(
+        '''
+        CREATE DATABASE parser_db;
+        '''
+    )
+    cursor.fetchone()
+
+
 parser_path = os.path.join(os.path.dirname(__file__))
