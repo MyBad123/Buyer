207f3ef1c0d6610d77051da97da7081a7cc7aa47
Sergey Zadorojniy <szadorojniy@1d61.com>
2022-05-13 12:44:01 +0000

    Update parser_starting.py

--- htmlTree/ParseLib/parser_starting.py
+++ htmlTree/ParseLib/parser_starting.py
@@ -4,6 +4,6 @@ from .Functions.Parser import Parser
 
 def start(path):
     parser = Parser()
-    path = parser.site_parsing("https://allflorarium.ru/", str(uuid.uuid4()), path)
+    path = parser.site_parsing(url, str(uuid.uuid4()), path)
 
     return path
