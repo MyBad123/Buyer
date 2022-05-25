4a8eb02981198274461bcb1f4b23f61293db3473
Sergey Zadorojniy <szadorojniy@1d61.com>
2022-05-25 19:12:11 +0000

    Update update_buyerdev.sh

--- update_buyerdev.sh
+++ update_buyerdev.sh
@@ -3,14 +3,14 @@
 #перейти в каталог с проектом
 cd /
 cd /home/itsuppus/svetorezerv/buyer/
-#git pull
+git pull
 #Удалить лишние миграции
 sudo rm -f /home/itsuppus/svetorezerv/buyer/app/migrations/0001_initial.py
 
 #создаём базу
 sudo -u postgres psql -f init.sql
 #восстанавливаем зависимости
-/home/itsuppus/svetorezerv/buyer/venv/bin/python pip3 install -r ./requirements.txt
+/home/itsuppus/svetorezerv/buyer/venv/bin/python /home/itsuppus/svetorezerv/buyer/venv/bin/pip3 install -r ./requirements.txt
 #обновить миграции
 /home/itsuppus/svetorezerv/buyer/venv/bin/python manage.py makemigrations
 /home/itsuppus/svetorezerv/buyer/venv/bin/python manage.py migrate
