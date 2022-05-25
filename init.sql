708cf690214b4e52b2ac357cdf45f0ab90e8b3a7
Sergey Zadorojniy <szadorojniy@1d61.com>
2022-05-25 19:03:59 +0000

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,3 +1,7 @@
+UPDATE pg_database SET datallowconn = 'false' WHERE datname = 'b2b';
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'b2b' AND pid <> pg_backend_pid();
 DROP DATABASE "b2b";
 CREATE DATABASE b2b;
 ALTER ROLE buyer_user SET client_encoding TO 'utf8';
