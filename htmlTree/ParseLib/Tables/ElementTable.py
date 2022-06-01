c029fb1485d010e5e7c1425b7bf5f11c80bf2bb7
Titov Pavel <ptitov@1d61.com>
2022-06-01 12:23:49 +0300

    Changed columns in ElementTable.py

--- htmlTree/ParseLib/Tables/ElementTable.py
+++ htmlTree/ParseLib/Tables/ElementTable.py
@@ -15,7 +15,6 @@ class ElementTable(TemplateTable):
 
     def columns(self):
         return {"id": ["SERIAL", "PRIMARY KEY"],
-                "el_order": ["integer"],
                 "content_element": ["text"],
                 "url": ["text"],
                 "length": ["integer"],
