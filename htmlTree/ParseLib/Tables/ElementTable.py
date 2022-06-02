157989278b1fc08cb38d44e30782e17d879b21fb
Titov Pavel <ptitov@1d61.com>
2022-06-02 13:20:47 +0300

    Fixed bugs

--- htmlTree/ParseLib/Tables/ElementTable.py
+++ htmlTree/ParseLib/Tables/ElementTable.py
@@ -1,15 +1,7 @@
 from htmlTree.ParseLib.Tables.TemplateTable import TemplateTable
-from htmlTree.ParseLib.Postgre.postgre_connection import PostgreConnector
 
 
 class ElementTable(TemplateTable):
-    connection = PostgreConnector()
-
-    def __init__(self, name):
-        self.dbConnection = self.connection
-        self.name = name
-        return
-
     def table_name(self):
         return "elements" + f"_{self.name}"
 
--- htmlTree/ParseLib/Tables/TemplateTable.py
+++ htmlTree/ParseLib/Tables/TemplateTable.py
@@ -8,8 +8,9 @@ from htmlTree.ParseLib.Postgre.postgre_connection import PostgreConnector
 class TemplateTable:
     connection = PostgreConnector()
 
-    def __init__(self):
+    def __init__(self, name):
         self.dbConnection = self.connection
+        self.name = name
         return
 
     def table_name(self):
