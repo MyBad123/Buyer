fe633c403d3bf06106fc32903a990dc86ec98aaf
Titov Pavel <ptitov@1d61.com>
2022-06-02 11:44:41 +0300

    Delete config.yaml and add to connector attributes from env

--- htmlTree/ParseLib/Postgre/postgre_connection.py
+++ htmlTree/ParseLib/Postgre/postgre_connection.py
@@ -8,14 +8,11 @@ from htmlTree.ParseLib.parser_path import parser_path
 
 class PostgreConnector:
     def __init__(self):
-        with open(parser_path + '/config.yaml') as f:
-            config = yaml.safe_load(f)
-            print(config['host'])
-            self.conn = psycopg2.connect(user=config['user'],
-                                         password=config['password'],
-                                         host=config['host'],
-                                         port=config['port'],
-                                         database=config['database'])
+        self.conn = psycopg2.connect(user=os.environ['SQL_USER'],
+                                     password=os.environ['SQL_PASSWORD'],
+                                     host=os.environ['SQL_HOST'],
+                                     port=os.environ['SQL_PORT'],
+                                     database=os.environ['SQL_DATABASE'])
 
     def __del__(self):
         if self.conn:
