98be64ad0990aebe2a89484d93f951bfabc8df1b
Titov Pavel <ptitov@1d61.com>
2022-06-02 12:06:21 +0300

    adding to connector attributes from env

--- htmlTree/ParseLib/Postgre/postgre_connection.py
+++ htmlTree/ParseLib/Postgre/postgre_connection.py
@@ -1,13 +1,19 @@
 import os
+import pathlib
+from dotenv import load_dotenv
 
 import psycopg2
-import yaml
 
 from htmlTree.ParseLib.parser_path import parser_path
 
 
 class PostgreConnector:
     def __init__(self):
+        # work with env
+        path_my_my = str(pathlib.Path(__file__).parent.parent.parent.parent) + '/Buyer/'
+        dotenv_path = os.path.join(path_my_my, '.env')
+        if os.path.exists(dotenv_path):
+            load_dotenv(dotenv_path)
         self.conn = psycopg2.connect(user=os.environ.get('SQL_USER'),
                                      password=os.environ.get('SQL_PASSWORD'),
                                      host=os.environ.get('SQL_HOST'),
