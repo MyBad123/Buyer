c2877012647b565a15f28a29e4e39e1844ce2e2f
Sergey Zadorojniy <szadorojniy@1d61.com>
2022-06-05 11:18:35 +0000

    Update update_buyerdev.sh

--- update_buyerdev.sh
+++ update_buyerdev.sh
@@ -3,7 +3,9 @@
 #перейти в каталог с проектом
 cd /
 cd /home/itsuppus/svetorezerv/buyer/
-git pull -f
+git stash
+git stash drop
+git pull
 #Удалить лишние миграции
 sudo rm -f /home/itsuppus/svetorezerv/buyer/app/migrations/0001_initial.py
 
